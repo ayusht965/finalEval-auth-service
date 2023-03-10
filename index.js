260e472ea2f9c9c4b7a5287b07480dc3e163386d
Ayush Tyagi <ak7046243581@gmail.com>
2023-03-10 22:30:50 +0530

    chore: add cors to auth

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
+const cors = require('cors');
 
 const {PORT} = require('./config');
 
 const express = require('express');
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 const apiRoutes = require('./src/routes/authRoutes');
